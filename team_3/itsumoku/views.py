9704c1c827d11bd7c94060a1fc94926d027dcde2
yoshi429 <1gk6204y@komazawa-u.ac.jp>
2021-08-04 15:33:23 +0900

    ver1.00

--- team_3/itsumoku/views.py
+++ team_3/itsumoku/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+from itsumoku.app.getmov import exec_getmov
+
+
+DEFAULT_KEYWORD = 'もくもく会'
+
+
 def index(request):
+    if request.method == 'POST':
+        
+        during = request.POST['during']
+        any = request.POST['keyword']
+        male = request.POST['keyword']
+        woman = request.POST['keyword']
+        anime = request.POST['keyword']
+
+        keyword_list = [DEFAULT_KEYWORD, any, male, woman, anime ]
+        keyword = ' '.join(keyword_list)
+
+        context = {
+            'df_video_list':  exec_getmov(during, keyword)
+        }
+        return render(request, 'result.html', context)
+
     return render(request, 'index.html')
+
+
